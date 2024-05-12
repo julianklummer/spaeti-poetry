c7800306e32f001d252fdca7750e9aaf956dfe4b
Julian Klummer <secure@julianklummer.de>
2024-05-12 22:34:24 +0200

    add test pages with submission

---
+++ next/app/admin/page.tsx
@@ -0,0 +1,18 @@
+import { getStore } from "@netlify/blobs";
+
+async function getData(): Promise<any[]> {
+  const store = getStore("constributions");
+  const contributionList: any[] = [];
+
+  for await (const entry of store.list({ paginate: true })) {
+    entry.blobs.forEach((blob) => contributionList.push(blob));
+  }
+
+  return contributionList;
+}
+
+export async function Page() {
+  const data = await getData();
+
+  return <>{JSON.stringify(data)}</>;
+}
