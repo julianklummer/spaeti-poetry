012cb035b8ee3ef80b48d2283a516a8a8532d387
Julian Klummer <secure@julianklummer.de>
2024-05-14 22:57:30 +0200

    test

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -3,24 +3,24 @@ import styles from "./page.module.css";
 
 export const dynamic = "force-dynamic";
 
-const getData = async () => {
-  const res = await fetch(`${process.env.SITE_BASE_URL}/api/contributions`, {
-    cache: "no-cache",
-  });
-  const { contributionList } = await res.json();
-  return contributionList;
-};
+// const getData = async () => {
+//   const res = await fetch(`${process.env.SITE_BASE_URL}/api/contributions`, {
+//     cache: "no-cache",
+//   });
+//   const { contributionList } = await res.json();
+//   return contributionList;
+// };
 
 export default async function Home() {
-  const contributionList: any[] = await getData();
+  // const contributionList: any[] = await getData();
 
   return (
     <main className={styles.main}>
-      <ul>
+      {/* <ul>
         {contributionList.map((contribution, index) => {
           return <li key={index}>{JSON.stringify(contribution)}</li>;
         })}
-      </ul>
+      </ul> */}
       <form action={handleSubmission}>
         <input type="text" name="text" />
         <button type="submit">Submit</button>
