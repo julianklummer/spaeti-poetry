90607e5144b22591cc702402d5c6fddc357f1586
Julian Klummer <secure@julianklummer.de>
2024-05-14 21:58:59 +0200

    unset cahce

--- next/app/actions.ts
+++ next/app/actions.ts
@@ -1,26 +1,13 @@
 "use server";
 
-import { getStore } from "@netlify/blobs";
 import { revalidatePath } from "next/cache";
 
-const store = getStore({
-  name: "constributions",
-  siteID: process.env.NETLIFY_SITE_ID,
-  token: process.env.NETLIFY_ACCESS_TOKEN,
-});
-
 export const getContributionList = async (): Promise<any[]> => {
-  const res = await fetch(`${process.env.SITE_BASE_URL}/api/contributions`);
+  const res = await fetch(`${process.env.SITE_BASE_URL}/api/contributions`, {
+    cache: "no-cache",
+  });
   const { contributionList } = await res.json();
   return contributionList;
-};
-
-export const submitContribution = async (text: string) => {
-  const timeStamp = new Date().getTime();
-
-  if (text) {
-    await store.set(String(timeStamp), text);
-  }
 };
 
 export const handleSubmission = async (formData: FormData) => {
